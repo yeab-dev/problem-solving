80d5b1ca2b7e28144430c03a779a891539e10fc3
yeabsera <yeab.dev@gmail.com>
2025-03-24 17:52:26 +0300

    solve problem #9 how it was supposed to be solved

--- leetcode/palindrome_number.py
+++ leetcode/palindrome_number.py
@@ -1,3 +1,9 @@
 class Solution:
     def isPalindrome(self, x: int) -> bool:
-        return(str(x) == str(x)[::-1])
+        if x % 10 == 0 and x != 0:
+            return False
+        remainder = 0
+        while x > remainder:
+            remainder = remainder * 10 + x % 10
+            x = x // 10
+        return x == remainder or x == remainder // 10
